d3ebf75ed432a79aef65aa0bfdc7177cf3872c45
shinji hayamizu <max9173627@gmail.com>
2022-03-31 19:47:35 +0900

    userState作成

---
+++ src/features/userSlice.ts
@@ -0,0 +1,27 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { RootState } from "../app/store";
+
+export const userSlice = createSlice({
+  name: "user",
+  initialState: {
+    user: { uid: "", photoUrl: "", displayName: "" },
+  },
+  reducers: {
+    login: (state, action) => {
+      state.user = action.payload;
+    },
+    logout: (state) => {
+      state.user = { uid: "", photoUrl: "", displayName: "" };
+    },
+    // updateUserProfile: (state, action: PayloadAction<USER>) => {
+    //   state.user.displayName = action.payload.displayName;
+    //   state.user.photoUrl = action.payload.photoUrl;
+    // },
+  },
+});
+
+export const { login, logout } = userSlice.actions;
+
+export const selectCount = (state: RootState) => state.user.user;
+
+export default userSlice.reducer;
